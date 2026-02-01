6b24fb402b279b6ada6cba55bbbcd8c7b5a50868
zagani212 <abdelhakassahi@gmail.com>
2026-02-01 19:27:17 +0100

    open port 3000

--- terraform/security/main.tf
+++ terraform/security/main.tf
@@ -15,6 +15,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_port" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_port_3000" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3000
+  ip_protocol       = "tcp"
+  to_port           = 3000
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_http_port" {
   security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
